4f25dd6fe7b1e558daebda030f511d6cb6935db1
Aung Yee <aung.yee@mytutor.co.uk>
2023-11-18 00:42:42 +0000

    hw0

--- Math2001/Homework/hw0.lean
+++ Math2001/Homework/hw0.lean
@@ -10,4 +10,11 @@ attribute [-instance] Int.instDivInt_1 Int.instDivInt Nat.instDivNat
 
 /- 5 points -/
 theorem problem1 {n : ℤ} (hn : n ≥ 5) : n ^ 2 > 2 * n + 11 :=
-  sorry
+calc
+  n ^ 2
+    = n * n := by ring
+  _ ≥ 5 * n := by rel [hn]
+  _ = 2 * n + 3 * n := by ring
+  _ ≥ 2 * n + 3 * 5 := by rel [hn]
+  _ = 2 * n + 11 + 4 := by ring
+  _ > 2 * n + 11 := by extra
